9bc71fb00dd9a73f4db2a0ea9af0a5be9bcc82eb
mohanjay112 <mohanjay112@gmail.com>
2024-12-21 21:41:38 +0530

    change tomcat user data

--- userdata/tomcat-provision.sh
+++ userdata/tomcat-provision.sh
@@ -1,89 +1,72 @@
 #!/bin/bash
+
+# Update the system
 sudo apt update
-sudo apt install openjdk-8-jdk -y
+
+# Install JDK 17
+sudo apt install openjdk-17-jdk -y
+
+# Install other necessary utilities
 sudo apt install git wget unzip -y
 sudo apt install awscli -y
-TOMURL="https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.37/bin/apache-tomcat-8.5.37.tar.gz"
+
+# Download Tomcat 9
+TOMURL="https://archive.apache.org/dist/tomcat/tomcat-9/v9.0.62/bin/apache-tomcat-9.0.62.tar.gz"
 cd /tmp/
 wget $TOMURL -O tomcatbin.tar.gz
+
+# Extract the downloaded file
 EXTOUT=`tar xzvf tomcatbin.tar.gz`
 TOMDIR=`echo $EXTOUT | cut -d '/' -f1`
+
+# Add tomcat user
 useradd --shell /sbin/nologin tomcat
-rsync -avzh /tmp/$TOMDIR/ /usr/local/tomcat8/
-rm -rf /usr/local/tomcat8/conf/tomcat-users.xml
-rm -rf /usr/local/tomcat8/webapps/manager/META-INF/context.xml
-touch /usr/local/tomcat8/webapps/manager/META-INF/context.xml
-touch /usr/local/tomcat8/conf/tomcat-users.xml
-cat <<EOT>> /usr/local/tomcat8/conf/tomcat-users.xml
+
+# Install Tomcat to the correct directory
+rsync -avzh /tmp/$TOMDIR/ /usr/local/tomcat9/
+
+# Clean up default Tomcat configuration files
+rm -rf /usr/local/tomcat9/conf/tomcat-users.xml
+rm -rf /usr/local/tomcat9/webapps/manager/META-INF/context.xml
+
+# Create new configuration files
+touch /usr/local/tomcat9/webapps/manager/META-INF/context.xml
+touch /usr/local/tomcat9/conf/tomcat-users.xml
+
+# Add necessary roles and users to tomcat-users.xml
+cat <<EOT>> /usr/local/tomcat9/conf/tomcat-users.xml
 <?xml version="1.0" encoding="UTF-8"?>
-<!--
-  Licensed to the Apache Software Foundation (ASF) under one or more
-  contributor license agreements.  See the NOTICE file distributed with
-  this work for additional information regarding copyright ownership.
-  The ASF licenses this file to You under the Apache License, Version 2.0
-  (the "License"); you may not use this file except in compliance with
-  the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
--->
 <tomcat-users xmlns="http://tomcat.apache.org/xml"
 xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 xsi:schemaLocation="http://tomcat.apache.org/xml tomcat-users.xsd"
 version="1.0">
-<!--
-  NOTE:  By default, no user is included in the "manager-gui" role required
-  to operate the "/manager/html" web application.  If you wish to use this app,
-  you must define such a user - the username and password are arbitrary. It is
-  strongly recommended that you do NOT use one of the users in the commented out
-  section below since they are intended for use with the examples web
-  application.
--->
-<!--
-  NOTE:  The sample user and role entries below are intended for use with the
-  examples web application. They are wrapped in a comment and thus are ignored
-  when reading this file. If you wish to configure these users for use with the
-  examples web application, do not forget to remove the <!.. ..> that surrounds
-  them. You will also need to set the passwords to something appropriate.
--->
   <role rolename="manager-gui"/>
   <role rolename="manager-script"/>
   <user username="tomcat" password="admin123" roles="manager-gui,manager-script"/>
 </tomcat-users>
 EOT
 
-cat <<EOT>> /usr/local/tomcat8/webapps/manager/META-INF/context.xml
+# Add configuration to context.xml
+cat <<EOT>> /usr/local/tomcat9/webapps/manager/META-INF/context.xml
 <?xml version="1.0" encoding="UTF-8"?>
-<!--
-  Licensed to the Apache Software Foundation (ASF) under one or more
-  contributor license agreements.  See the NOTICE file distributed with
-  this work for additional information regarding copyright ownership.
-  The ASF licenses this file to You under the Apache License, Version 2.0
-  (the "License"); you may not use this file except in compliance with
-  the License.  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
--->
 <Context antiResourceLocking="false" privileged="true" >
-<!--
-  <Valve className="org.apache.catalina.valves.RemoteAddrValve"
-         allow="127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1" />
-  <Manager sessionAttributeValueClassNameFilter="java\.lang\.(?:Boolean|Integer|Long|Number|String)|org\.apache\.catalina\.filters\.CsrfPreventionFilter\$LruCache(?:\$1)?|java\.util\.(?:Linked)?HashMap"/>
--->
 </Context>
 EOT
-chown -R tomcat.tomcat /usr/local/tomcat8 
+
+# Change ownership to tomcat user
+chown -R tomcat.tomcat /usr/local/tomcat9
+
+# Create a setenv.sh file to configure JVM arguments
+cat <<EOT>> /usr/local/tomcat9/bin/setenv.sh
+#!/bin/bash
+# Set JVM options (adjust values as needed)
+export CATALINA_OPTS="-Xms512m -Xmx1024m -Dmyproperty=value"
+EOT
+
+# Make the setenv.sh file executable
+chmod +x /usr/local/tomcat9/bin/setenv.sh
+
+# Create a systemd service for Tomcat
 cat <<EOT>> /etc/systemd/system/tomcat.service
 [Unit]
 Description=Tomcat
@@ -91,17 +74,20 @@ After=network.target
 
 [Service]
 User=tomcat
-WorkingDirectory=/usr/local/tomcat8
-Environment=CATALINA_HOME=/usr/local/tomcat8
-Environment=CATALINE_BASE=/usr/local/tomcat8
-ExecStart=/usr/local/tomcat8/bin/catalina.sh run
-ExecStop=/usr/local/tomcat8/bin/shutdown.sh
+WorkingDirectory=/usr/local/tomcat9
+Environment=CATALINA_HOME=/usr/local/tomcat9
+Environment=CATALINA_BASE=/usr/local/tomcat9
+ExecStart=/usr/local/tomcat9/bin/catalina.sh run
+ExecStop=/usr/local/tomcat9/bin/shutdown.sh
 SyslogIdentifier=tomcat-%i
 
 [Install]
 WantedBy=multi-user.target
 EOT
 
+# Reload systemd to recognize the new service
 systemctl daemon-reload
+
+# Start and enable Tomcat to run on boot
 systemctl start tomcat
 systemctl enable tomcat
